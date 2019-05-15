fee63d914f90fc61ea8f963b57bc7b551282ca87
Francis de Ladurantaye <fdl9044@gmail.com>
2019-05-15 14:51:09 -0400

    fixed another issue with numpy seed

--- gan_model.py
+++ gan_model.py
@@ -19,15 +19,14 @@ from score_fid import get_fid_score
 class GAN_Model(torch.nn.Module):
     def __init__(self, numpy_initial_seed, torch_initial_seed):
         super(GAN_Model, self).__init__()
+        # set and save initial seeds for reproducibility
+        np.random.seed(numpy_initial_seed)
+        torch.manual_seed(torch_initial_seed)
+        self.numpy_seed = np.random.get_state()
+        self.torch_seed = torch.initial_seed()
         # save best fid and training epochs count
         self.best_fid = float('Inf')
         self.n_epochs = 0
-        # save seeds for reproducitility
-        self.numpy_seed = numpy_initial_seed
-        self.torch_seed = torch_initial_seed
-        # set initial seeds for reproducibility
-        np.random.seed(numpy_initial_seed)
-        torch.manual_seed(torch_initial_seed)
         # batch of samples for visuals creation
         self.sample_batch = torch.randn(64, z_size).to(device)
 
